25ad19a44e1d24ec527f8a70615e845d4252bb97
matsukawa06 <rice_shower_35@hotmail.com>
2021-11-27 18:04:22 +0900

    ハンバーガー

---
+++ js/common.js
@@ -0,0 +1,19 @@
+(function($) {
+    var $body = $('body');
+    var $btn = $('.toggle_btn');
+    var $mask = $('#mask');
+    var open = 'open'; // class
+    // menu open close
+    $btn.on('click', function() {
+        alert("test");
+        if ( ! $body.hasClass( open ) ) {
+            $body.addClass( open );
+        } else {
+            $body.removeClass( open );
+        }
+    });
+    // mask close
+    $mask.on('click', function() {
+        $body.removeClass( open );
+    });
+} )(jQuery);
